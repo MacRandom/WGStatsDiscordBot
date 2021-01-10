a9635bc11170816371cfb12cad43f1ef326ff194
MacRandom <macrandomstudio@gmail.com>
2021-01-10 16:36:57 +0700

    Добавлен метод получения игрока.

--- WotStatBot/Wargaming/WargamingApi.cs
+++ WotStatBot/Wargaming/WargamingApi.cs
@@ -1,7 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using WotStatBot.Models;
 
 namespace WotStatBot.Wargaming
 {
@@ -16,6 +18,37 @@ namespace WotStatBot.Wargaming
             _configuration = configuration;
             _appId = _configuration["Wargaming:AppId"];
             _urlConstructor = new UrlConstructor(_appId);
+        }
+
+        public async Task<Player> GetPlayer(string name)
+        {
+            HttpClient client = new HttpClient();
+            string urlRequest = _urlConstructor.GetFindPlayerUrl(name);
+
+            var response = await client.GetAsync(urlRequest);
+            var responseString = await response.Content.ReadAsStringAsync();
+            dynamic parsed = JsonConvert.DeserializeObject(responseString);
+
+            if (parsed.status == "ok")
+            {
+                int count = parsed.meta.count;
+                if (count > 0)
+                {
+                    return new Player
+                    { 
+                        Id = parsed.data[0].account_id,
+                        Name = parsed.data[0].nickname
+                    };
+                }
+                else
+                {
+                    throw new Exception("Игрок не найден");
+                }
+            }
+            else
+            {
+                throw new Exception("Неверный запрос");
+            }
         }
     }
 }
