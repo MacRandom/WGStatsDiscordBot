16de67bb61cf43479fd64451fe2361588f2516e2
MacRandom <macrandomstudio@gmail.com>
2021-01-10 13:36:38 +0700

    Добавлен файл конфигурации и запуск бота.

---
+++ WotStatBot/BotCore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace WotStatBot
+{
+    internal class BotCore
+    {
+        private IConfigurationRoot _configuration;
+
+        public BotCore(IConfigurationRoot configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task Start()
+        {
+
+        }
+    }
+}
--- WotStatBot/Program.cs
+++ WotStatBot/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace WotStatBot
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("configuration.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var bot = new BotCore(configuration);
+
+            await bot.Start();
         }
     }
 }
