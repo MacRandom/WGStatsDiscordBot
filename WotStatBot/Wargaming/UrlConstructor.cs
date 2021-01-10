ede4e8dce80893dd38caef84b770000abd622823
MacRandom <macrandomstudio@gmail.com>
2021-01-10 15:19:55 +0700

    Добавлен класс запросов к api Wargaming и класс для построения url этих запросов.

---
+++ WotStatBot/Wargaming/UrlConstructor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WotStatBot.Wargaming
+{
+    internal class UrlConstructor
+    {
+        private const string _wotUrlPrefix = @"https://api.worldoftanks.ru/wot/";
+        private const string _listSuffix = @"account/list/";
+        private const string _infoSuffix = @"account/info/";
+        private const string _appIdField = @"?application_id=";
+        private const string _searchField = @"&search=";
+        private string _appId;
+
+        public UrlConstructor(string appId)
+        {
+            _appId = appId;
+        }
+
+        public string GetFindPlayerUrl(string name)
+            => _wotUrlPrefix + _listSuffix + _appIdField + _appId + _searchField + name;
+    }
+}
---
+++ WotStatBot/Wargaming/WargamingApi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace WotStatBot.Wargaming
+{
+    internal class WargamingApi
+    {
+        private IConfigurationRoot _configuration;
+        private string _appId;
+        private UrlConstructor _urlConstructor;
+
+        public WargamingApi(IConfigurationRoot configuration)
+        {
+            _configuration = configuration;
+            _appId = _configuration["Wargaming:AppId"];
+            _urlConstructor = new UrlConstructor(_appId);
+        }
+    }
+}
