3da87769ec4d25caf333612f5cc0968d53305ec6
MacRandom <macrandomstudio@gmail.com>
2021-01-10 20:16:51 +0700

    Добавлена команда для получения статистики по игроку.

--- WotStatBot/Modules/BotCommands.cs
+++ WotStatBot/Modules/BotCommands.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using WotStatBot.Models;
@@ -25,6 +26,32 @@ namespace WotStatBot.Modules
             await context.TriggerTypingAsync();
 
             await context.RespondAsync($"Name: {player.Name}, Id: {player.Id}");
+        }
+
+        [Command("stat")]
+        public async Task GetStat(CommandContext context, string name)
+        {
+            var api = context.Dependencies.GetDependency<WargamingApi>();
+
+            var player = await api.GetPlayer(name);
+
+            var playerStat = await api.GetStat(player.Id);
+
+            await context.TriggerTypingAsync();
+
+            await context.RespondAsync(
+                $"Статистика игрока {player.Name} в случайных боях{Environment.NewLine}" +
+                $"Провёл {playerStat.BattleCount} боёв, из них побед - {playerStat.WinCount}, ничьих - {playerStat.DrawCount}, поражений - {playerStat.LoseCount}{Environment.NewLine}" +
+                $"Выжил в {playerStat.SurviveCount} боях{Environment.NewLine}" +
+                $"Процент побед {(float)playerStat.WinCount * 100 / playerStat.BattleCount}%{Environment.NewLine}" +
+                $"Средние показатели за бой: {Environment.NewLine}" +
+                $"Средний урон: {(float)playerStat.DamageDealt / playerStat.BattleCount}{Environment.NewLine}" +
+                $"Средний ассист: {playerStat.AverageDamageAssisted}{Environment.NewLine}" +
+                $"Средний засвет: {playerStat.AverageDamageAssistedRadio}{Environment.NewLine}" +
+                $"Среднее гусление: {playerStat.AverageDamageAssistedTrack}{Environment.NewLine}" +
+                $"Процент попаданий: {playerStat.HitsPercent}%{Environment.NewLine}" +
+                $"Убил врагов: {playerStat.FragsCount}{Environment.NewLine}" +
+                $"Убил деревьев: {playerStat.TreesCut}{Environment.NewLine}");
         }
     }
 }
