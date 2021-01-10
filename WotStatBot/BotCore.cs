e422aefe7bbca5313de0cb2e7ed74d0d0873bff8
MacRandom <macrandomstudio@gmail.com>
2021-01-10 14:07:53 +0700

    Добавлен метод сборки зависимостей бота.

--- WotStatBot/BotCore.cs
+++ WotStatBot/BotCore.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
 using Microsoft.Extensions.Configuration;
 
 namespace WotStatBot
@@ -11,6 +14,9 @@ namespace WotStatBot
     {
         private IConfigurationRoot _configuration;
         private CancellationTokenSource _cancellationToken;
+        private DiscordClient _discordClient;
+        private InteractivityModule _interactivity;
+        private CommandsNextModule _commands;
 
         public BotCore(IConfigurationRoot configuration, CancellationTokenSource cancelationToken)
         {
@@ -21,6 +27,18 @@ namespace WotStatBot
         public async Task Start()
         {
 
+        }
+
+        private DependencyCollection BuildDeps()
+        {
+            using var deps = new DependencyCollectionBuilder();
+
+            deps.AddInstance(_interactivity)
+                .AddInstance(_cancellationToken)
+                .AddInstance(_configuration)
+                .AddInstance(_discordClient);
+
+            return deps.Build();
         }
     }
 }
