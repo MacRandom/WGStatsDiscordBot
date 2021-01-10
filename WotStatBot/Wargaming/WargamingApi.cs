431bd091f057655f308e06c89d8c5991fdb4f845
MacRandom <macrandomstudio@gmail.com>
2021-01-10 17:32:56 +0700

    Метод api для получения статистики по игроку.

--- WotStatBot/Wargaming/WargamingApi.cs
+++ WotStatBot/Wargaming/WargamingApi.cs
@@ -35,9 +35,50 @@ namespace WotStatBot.Wargaming
                 if (count > 0)
                 {
                     return new Player
-                    { 
+                    {
                         Id = parsed.data[0].account_id,
                         Name = parsed.data[0].nickname
+                    };
+                }
+                else
+                {
+                    throw new Exception("Игрок не найден");
+                }
+            }
+            else
+            {
+                throw new Exception("Неверный запрос");
+            }
+        }
+
+        public async Task<PlayerStats> GetStat(string id)
+        {
+            HttpClient client = new HttpClient();
+            string urlRequest = _urlConstructor.GetPlayerStatsRandomUrl(id);
+
+            var response = await client.GetAsync(urlRequest);
+            var responseString = await response.Content.ReadAsStringAsync();
+            dynamic parsed = JsonConvert.DeserializeObject(responseString);
+
+            if (parsed.status == "ok")
+            {
+                int count = parsed.meta.count;
+                if (count > 0)
+                {
+                    return new PlayerStats
+                    {
+                        TreesCut = parsed.data[0].statistics.random.avg_damage_assisted,
+                        AverageDamageAssisted = parsed.data[0].statistics.random.avg_damage_assisted,
+                        AverageDamageAssistedRadio = parsed.data[0].statistics.random.avg_damage_assisted_radio,
+                        AverageDamageAssistedTrack = parsed.data[0].statistics.random.avg_damage_assisted_track,
+                        AverageDamageBlocked = parsed.data[0].statistics.random.avg_damage_blocked,
+                        BattleCount = parsed.data[0].statistics.random.battles,
+                        DrawCount = parsed.data[0].statistics.random.draws,
+                        FragsCount = parsed.data[0].statistics.random.frags,
+                        HitsPercent = parsed.data[0].statistics.random.hits_percents,
+                        LoseCount = parsed.data[0].statistics.random.losses,
+                        SurviveCount = parsed.data[0].statistics.random.survived_battles,
+                        WinCount = parsed.data[0].statistics.random.wins
                     };
                 }
                 else
