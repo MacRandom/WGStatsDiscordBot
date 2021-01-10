cb33fc76dbfa2afbf18517b0f44bee329b239c32
MacRandom <macrandomstudio@gmail.com>
2021-01-10 14:23:20 +0700

    Добавлен модуль команд и тестовая команда.

---
+++ WotStatBot/Modules/BotCommands.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace WotStatBot.Modules
+{
+    public class BotCommands
+    {
+        [Command("alive")]
+        [Description("Bot is runnung test command")]
+        public async Task Alive(CommandContext context)
+        {
+            await context.TriggerTypingAsync();
+
+            await context.RespondAsync("I'm alive!");
+        }
+    }
+}
